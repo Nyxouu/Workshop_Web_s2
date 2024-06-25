3c2dab97737f4fca6e9a545b106cf714f6784f9f
Hellwyn <elvinkauffmann@gmail.com>
2024-06-25 14:29:54 +0200

    connection et formatage

--- Workshop_web_s2/model.py
+++ Workshop_web_s2/model.py
@@ -1,16 +1,34 @@
 import mysql.connector
 
-
-def get_all_games() :
+def format_query(keys, values):
+    column_names = [field[0] for field in keys]
+    result = []
+    for row in values:
+        row_dict = dict(zip(column_names, row))
+        result.append(row_dict)
+    return result
+    
+def connect():
     speedscore_db = mysql.connector.connect(
         host="localhost",
         user="root",
         password="",
         database="workshop-web-s2"
     )
+    return speedscore_db
+
+
+def get_all_games() :
+    connection = connect()
     SQL = "SELECT * FROM game"
-    bd_cursor = speedscore_db.cursor()
+    bd_cursor = connection.cursor()
     bd_cursor.execute(SQL)
-    data = bd_cursor.fetchall()
-    print(data)
-    return data
+
+    data = bd_cursor.fetchall() #values
+    fields_list = bd_cursor.description   # keys
+
+    formated_games = format_query(fields_list, data)
+
+    bd_cursor.close()
+    connection.close()
+    return formated_games
